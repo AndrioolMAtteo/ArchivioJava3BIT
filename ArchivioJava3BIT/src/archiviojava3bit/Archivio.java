15fe1ce26de30a96bb4879f5f74fcaa60f5d1f06
lucaa <lucaa@LAPTOP-LTL7P7VS.home-life.hub>
2021-04-26 13:13:09 +0200

    Ho creato l'inserimento e il ricerca del mio codice in contemporanea con il prof durante la lezione

--- ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
@@ -1,4 +1,5 @@
 package archiviojava3bit;
+import java.util.ArrayList;
 
 /**
  * Contiene gli oggetti del programma.
@@ -9,5 +10,31 @@ package archiviojava3bit;
  * @author INSERISCI_NOME
  */
 public class Archivio {
+    private ArrayList<Cioccolato> archivio;
+    
+    // Costruttore 
+    public Archivio() {
+        // creo l'array list vuoto
+        archivio = new ArrayList(0);
+    }
+    
+    // metodo per inserire un Cioccolato
+    public void inserisci(Cioccolato cioccolato) {
+        // aggiungo l'oggetto ricevuto nell'ArrayList
+        archivio.add(cioccolato);
+    }
+    
+    //Metodo per ricerca in base al codice 
+    public Cioccolato ricerca(String codice) {
+        for(int i=0; i<archivio.size();i++) {
+            // prendiamo l'oggetto in posizione i
+            Cioccolato cioccolato = archivio.get(i);
+            if(cioccolato.getcodice()==codice) {
+                return cioccolato;
+            }
+        }
+        // se arrivo qui significa che il cioccolato non c'è.
+        return null;
+    }
     
 }
